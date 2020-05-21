06f633d46ef734b73d464829334ba7aa98db0f6f
Katja Stefan <katja.stefan18@gmail.com>
2020-05-21 18:17:04 +0200

    Added budget update & debugged calculate total method in budget controller

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // BUDGET CONTROLLER
 let budgetController = (function() {
 
+  let data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    },
+    budget: 0,
+    percentage: -1
+  };
+
   // data model for expenses and incomes
   let Expense = function(id, description, value) {
     this.id = id;
@@ -17,22 +30,11 @@ let budgetController = (function() {
   let calculateTotal = function(type) {
     let sum = 0;
     data.allItems[type].forEach(function(current) {
-        sum += current;
+        console.log('current', current);
+        sum += current.value;
+        console.log('sum', sum);
     });
     data.totals[type] = sum;
-  };
-
-  let data = {
-    allItems: {
-      exp: [],
-      inc: []
-    },
-    totals: {
-      exp: 0,
-      inc: 0
-    },
-    budget: 0,
-    percentage: -1
   };
 
   return {
@@ -102,7 +104,11 @@ let UIController = (function() {
     inputValue: ".add__value",
     inputButton: ".add__btn",
     incomeContainer: ".income__list",
-    expensesContainer: ".expenses__list"
+    expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage"
   };
 
   return {
@@ -147,6 +153,12 @@ let UIController = (function() {
       fieldsArray[0].focus();
 
     },
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -180,6 +192,7 @@ let appController = (function(budgetCtrl, UICtrl) {
     let budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
+    UICtrl.displayBudget(budget);
 
   };
 
